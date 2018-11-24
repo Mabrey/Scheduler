b6e7fe89e058cea96ac81b76e5a60279308c2724
Mabrey <32260556+Mabrey@users.noreply.github.com>
2018-11-23 23:47:12 -0800

    User Account creation implemented.
    User can create an account that will be inserted into the database. Users are unable to create usernames that already exist. Passwords are hashed and salted, and userIDs are generated uniquely.

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ const port = process.env.PORT || 5000;
 
 const sqlite3 = require('sqlite3').verbose();
 
-let db = connectToDatabase();
+let userCount;
+let db; 
+//= connectToDatabase();
+
+initialize();
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -36,19 +41,36 @@ app.post('/api/returnLogin', (req,res) =>{
 });
 
 app.post('/api/createAccount', async (req, res) =>{
-
+  /** 
+   * @param req is a request with a potenial username and password combination
+   * @param res is a response that indicates the status of the attempted account creation. 
+   * 
+   * The username is verified to be valid and available first.
+   * The password is verified to be valid.
+   * The password is passed into a hash function which returns the hash and salt.
+   * The user count is updated so we can assign the user to a unique ID, which is current count + 1.
+   * A SQL prepared statement controls the input we can insert. 
+   * Initially, we only input the userID, username, hash, salt, and authType (default: "user").
+   * The insert statement is then run with the user's parameters, and a status is returned and shown to the user.
+  */
   let username = req.body.credentials.username;
   let password = req.body.credentials.password;
 
-  //attemptAccountCreate(username, password);
+  
   try{
     await isUsernameValid(username);
     await isUsernameAvailable(username);
     await isPasswordValid(password);
-    let {hash, salt} = await hash(password);
-    console.log(hash);
-    console.log(salt);
-    res.send(`Hash is ${hash}`);
+    let hashResult = await hash(password);
+    await updateUserCount();
+
+    let sql = `INSERT INTO user(userID, username, firstName, lastName, passHash, passSalt, authType) 
+              VALUES(?,?,?,?,?,?,?)`;
+    let insertParameters = [userCount+1, username, "NULL", "NULL", hashResult.hash, hashResult.salt, "user"];
+    
+    let createStatus = await insertUserInDB(sql, insertParameters);
+
+    res.send(`${createStatus}`);
   } catch (error){
     res.send(error);
   }
@@ -56,18 +78,55 @@ app.post('/api/createAccount', async (req, res) =>{
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-function connectToDatabase() {
-  
-  let db = new sqlite3.Database('./db/scheduler.db', (err) => {
-    if (err) {
-      return console.error(err.message);
+async function initialize(){
+  db = await connectToDatabase();
+  userCount = await getUserCount();
+}
+
+function updateUserCount(){
+  return new Promise(async (resolve, reject) =>{
+    try{
+      userCount = await getUserCount();
+      resolve("Success");
     }
+    catch(error){
+      reject("Failed");
+    }
+  });
+};
 
-    console.log('Connected to DB');
+function connectToDatabase() {
+  return new Promise((resolve, reject) => {
+    let db = new sqlite3.Database('./db/scheduler.db', (err) => {
+      if (err) {
+        console.error(err.message);
+        reject(err);
+      }
+      else {
+        console.log('Connected to DB');
+        resolve(db);
+      } 
     })
-  return db;
- };
+  })
+};
 
+async function getUserCount() {
+  let sql = "SELECT MAX(userID) AS userCount FROM user";
+  let userCount = await getMaxUserID(sql);
+  return userCount;
+};
+
+function getMaxUserID(sql){
+  return new Promise((resolve, reject) => {
+    db.get(sql, (err, row) => {
+      if (err){
+        console.error(err.message);
+        reject(err);
+      }
+      else {resolve(row.userCount);}
+    });
+  })
+}
 
 function hash(password) {
   return new Promise((resolve, reject) => {
@@ -81,29 +140,62 @@ function hash(password) {
   })
 };
 
-function isUsernameAvailable(username){
+function checkPassword(password, hash){
   return new Promise((resolve, reject) => {
+    bcrypt.compare(password, hash, function(err, res) {
+        if(err)
+          reject("Password No Match");
+        else resolve("You logged in successfully");
+      })
+  })
+};
+
+
+
+async function isUsernameAvailable(username){
+  return new Promise(async(resolve, reject) => {
     //let db = connectToDatabase();
-    let sql = `SELECT username FROM user WHERE username = ?`;
+    let sql = `SELECT EXISTS(SELECT username FROM user WHERE username = ?) AS taken`;
     
-    let taken = db.get(sql, [username], (err, row) => {
-        if (err){
-          return console.error(err.message);
-        }
-        console.log(row);
-        return row;
-      });
+    let taken = await checkUsernameInDB(sql, username);
     
     console.log(`Username: ${username}`);
     console.log(`Taken: ${taken}`);
     if (!taken){
-      resolve();
+      resolve("hello");
     }
     else{
       console.log("Username not Available");
       reject("Username Not Available");
     }
   });
+};
+
+async function checkUsernameInDB(sql, username){
+  return new Promise((resolve, reject) => {
+    console.log("im trying");
+    db.get(sql, [username], (err, row) => {
+      if (err){
+        return console.error(err.message);
+      }
+      console.log(`row: ${row.taken}`);
+      resolve(row.taken);
+    });
+  });
+}
+
+function insertUserInDB(sql, params){
+  return new Promise((resolve, reject) => {
+    console.log("Inserting user: " + params[1]);
+    db.run(sql, params, function(err){
+      if (err){
+        reject("Cannot insert user.");
+      }
+      else{
+        resolve("User account created.")
+      }
+    });
+  })
 };
 
 function isUsernameValid(username){
